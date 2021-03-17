562014660269d9d1eed15ffb06fb8455e516dfb6
Jarrah <jarrah31@gmail.com>
2021-03-17 21:09:08 +0000

    Supports bots and fixes searching > 100 messages

--- cleaner.py
+++ cleaner.py
@@ -7,7 +7,6 @@ from pyrogram.raw.types import InputPeerSelf, InputMessagesFilterEmpty
 from pyrogram.raw.types.messages import ChannelMessages
 from pyrogram.errors import FloodWait, UnknownError
 
-
 API_ID = getenv('API_ID', None) or int(input('Enter your Telegram API id: '))
 API_HASH = getenv('API_HASH', None) or input('Enter your Telegram API hash: ')
 
@@ -16,11 +15,13 @@ app.start()
 
 
 class Cleaner:
-    def __init__(self, chats=None, search_limit=1000, delete_chunk_size=100):
+    def __init__(self, chats=None, search_chunk_size=1000, delete_chunk_size=100, search_limit=5000, offset=10):
         self.chats = chats or []
-        self.search_limit = search_limit
+        self.search_chunk_size = search_chunk_size
         self.delete_chunk_size = delete_chunk_size
-
+        self.search_limit = search_limit
+        self.offset = offset
+        
     @staticmethod
     def chunks(l, n):
         """Yield successive n-sized chunks from l.
@@ -41,11 +42,15 @@ class Cleaner:
 
     def select_groups(self):
         chats = self.get_all_chats()
-        groups = [c for c in chats if c.type in ('group', 'supergroup')]
+        groups = [c for c in chats if c.type in ('bot', 'group', 'supergroup')]
 
-        print('Delete all your messages in')
+        print('Delete all messages in which of the following groups:')
         for i, group in enumerate(groups):
-            print(f'  {i+1}. {group.title}')
+            if group.type == 'bot':
+                group_name = group.first_name
+            elif group.type in ('group', 'supergroup'):
+                group_name = group.title
+            print(f'  {i+1}. {group_name} ({group.type})')
 
         print(
             f'  {len(groups) + 1}. '
@@ -67,37 +72,64 @@ class Cleaner:
         else:
             self.chats = [groups[n - 1]]
 
-        groups_str = ', '.join(c.title for c in self.chats)
-        print(f'\nSelected {groups_str}.\n')
+        groups_str = ""
+        for c in self.chats:
+            if c.type == 'bot':
+                groups_str += f"{c.first_name}\n"
+            elif c.type in ('group', 'supergroup'):
+                groups_str += f"{c.title}\n"
+
+        print(f'\nDeleting messages from:\n{groups_str}')
 
     def run(self):
         for chat in self.chats:
             peer = app.resolve_peer(chat.id)
             message_ids = []
-            add_offset = 0
-
+            q = self.get_total_messages(peer)
+            add_offset = self.offset
+            chat_tot_messages = q.count
+            if chat.type == 'bot':
+                group_name = chat.first_name
+            elif chat.type in ('group', 'supergroup'):
+                group_name = chat.title
+            print(f'Found {chat_tot_messages} of your messages in "{group_name}"')
+            print(f'Offset by {add_offset} latest messages')
+            print(f'Searching up to the defined limit of {self.search_limit}')
             while True:
-                q = self.search_messages(peer, add_offset)
-                message_ids.extend(msg.id for msg in q['messages'])
-                messages_count = len(q['messages'])
-                print(f'Found {messages_count} of your messages in "{chat.title}"')
-                if messages_count < self.search_limit:
+                a = self.search_messages(chat.id, add_offset)
+                message_ids.extend(msg.message_id for msg in a)
+                tot_messages = len(message_ids)
+                messages_count = len(a)
+                if messages_count < self.search_chunk_size:
+                    print(f'Deleting {messages_count} messages...\n')
                     break
-                add_offset += self.search_limit
+                if tot_messages >= self.search_limit:
+                    print(f'Deleting {tot_messages} messages (defined search limit)...\n')
+                    break
+                add_offset += self.search_chunk_size
 
-            self.delete_messages(chat.id, message_ids)
+            # 
+            self.delete_messages(chat.id, message_ids, peer)
 
-    def delete_messages(self, chat_id, message_ids):
-        print(f'Deleting {len(message_ids)} messages with message IDs:')
-        print(message_ids)
+    def delete_messages(self, chat_id, message_ids, peer):
         for chunk in self.chunks(message_ids, self.delete_chunk_size):
             try:
                 app.delete_messages(chat_id=chat_id, message_ids=chunk)
             except FloodWait as flood_exception:
                 sleep(flood_exception.x)
+        sleep(1)
+        q = self.get_total_messages(peer)
+        print(f'{q.count} messages remaining...')
 
-    def search_messages(self, peer, add_offset):
-        print(f'Searching messages. OFFSET: {add_offset}')
+    def search_messages(self, chat_id, add_offset):
+        return app.search_messages(
+            chat_id, 
+            limit=self.search_chunk_size,
+            offset=add_offset
+        )
+        tot_messages = len(a)
+
+    def get_total_messages(self, peer):
         return app.send(
             Search(
                 peer=peer,
@@ -106,12 +138,12 @@ class Cleaner:
                 min_date=0,
                 max_date=0,
                 offset_id=0,
-                add_offset=add_offset,
-                limit=self.search_limit,
+                add_offset=0,
+                limit=1,
                 max_id=0,
                 min_id=0,
                 hash=0,
-                from_id=InputPeerSelf()
+                # from_id=InputPeerSelf()
             )
         )
 
